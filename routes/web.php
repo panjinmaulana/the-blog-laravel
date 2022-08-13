0ef8ff7d1a9447a78d722279491f9d4de3390843
Panji Maulana <panjinmaulana@gmail.com>
2022-08-13 22:08:34 +0700

    14. View Login & Registration

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\PostController;
 use App\Models\Category;
-use App\Models\User;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -78,3 +79,7 @@ Route::get('/categories', function() {
 //         'posts' => $author->posts->load('category', 'author'),
 //     ]);
 // });
+
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
